21ef8e4ef9e766a76bebae5ba659e752aeea2b2f
leonan <leonan.marques886@gmail.com>
2022-10-26 18:01:53 -0300

    método adicionar implementado

--- src/main/java/com/projeto/projetoapi/controller/ClienteController.java
+++ src/main/java/com/projeto/projetoapi/controller/ClienteController.java
@@ -2,19 +2,32 @@ package com.projeto.projetoapi.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projeto.projetoapi.model.Cliente;
+import com.projeto.projetoapi.repository.ClienteRepository;
 
 @RestController
-@RequestMapping("/clientes")
 public class ClienteController {
     
-    @GetMapping
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @RequestMapping(value = "/clientes", method = RequestMethod.GET)
     public List<Cliente> listar(){
-        return "Olá mundo";
+        return clienteRepository.findAll();
+    }
+
+    @RequestMapping(value = "/clientes", method = RequestMethod.POST)
+    public Cliente adicionar(@RequestBody Cliente cliente){
+        return clienteRepository.save(cliente);
     }
     
 }
