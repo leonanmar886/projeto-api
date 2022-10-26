fd84222238a0aa9fdc928a9cd3f7f27cccc28a2d
leonan <leonan.marques886@gmail.com>
2022-10-26 18:19:39 -0300

    Função encontrar pelo nome implementada

--- src/main/java/com/projeto/projetoapi/controller/ClienteController.java
+++ src/main/java/com/projeto/projetoapi/controller/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,12 @@ public class ClienteController {
     @RequestMapping(value = "/clientes", method = RequestMethod.POST)
     public Cliente adicionar(@RequestBody Cliente cliente){
         return clienteRepository.save(cliente);
+    }
+
+    @RequestMapping(value = "/clientes/{nome}", method = RequestMethod.GET)
+    public List<Cliente> procuraNome(@PathVariable(value = "nome") String nome){
+        List<Cliente> pessoas = clienteRepository.findByNome(nome);
+        return pessoas;
     }
     
 }
--- src/main/java/com/projeto/projetoapi/repository/ClienteRepository.java
+++ src/main/java/com/projeto/projetoapi/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.projeto.projetoapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.projeto.projetoapi.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-    
+    List<Cliente> findByNome(String nome);
 }
